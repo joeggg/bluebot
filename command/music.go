a1247c97870b1d737bd9d92b54b7cf98e2316b14
joeggg <ninjoetsu@gmail.com>
2022-06-01 22:28:27 +0100

    concurrent audio decode/encode + some refactor

--- command/music.go
+++ command/music.go
@@ -3,7 +3,9 @@ package command
 import (
 	"encoding/binary"
 	"io"
+	"log"
 	"os"
+	"sync"
 
 	"github.com/bwmarrin/discordgo"
 	"github.com/hajimehoshi/go-mp3"
@@ -16,33 +18,25 @@ var (
 	SampleRate int = 48000
 )
 
+func HandleYT(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
+	return nil
+}
+
 func HandleTell(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
-	// Find sender's voice channel
-	guild, err := session.State.Guild(msg.GuildID)
+	// Check the file opens first
+	file, err := os.Open("botsounds/output.mp3")
 	if err != nil {
 		return err
 	}
-	voicechannelID := ""
-	for _, vs := range guild.VoiceStates {
-		if vs.UserID == msg.Author.ID {
-			voicechannelID = vs.ChannelID
-			break
-		}
-	}
-	if voicechannelID == "" {
-		session.ChannelMessageSend(msg.ChannelID, "You're not in a voice channel")
-		return nil
-	}
+	defer file.Close()
 
-	// Join voice channel and start websocket audio communication
-	vc, err := session.ChannelVoiceJoin(msg.GuildID, voicechannelID, false, true)
+	vc, err := JoinAuthorVoiceChannel(session, msg)
 	if err != nil {
 		return err
 	}
 	// Any error handling past here must close the voice channel connection
-
 	vc.Speaking(true)
-	err = LoadAndPlay(vc, "botsounds/output.mp3")
+	err = PlayMP3(vc, file)
 	if err != nil {
 		vc.Disconnect()
 		return err
@@ -53,46 +47,114 @@ func HandleTell(session *discordgo.Session, msg *discordgo.MessageCreate, args [
 	return nil
 }
 
-func LoadAndPlay(vc *discordgo.VoiceConnection, filename string) error {
-	file, err := os.Open(filename)
+/*
+	Find if a the message author is in a channel and join it
+*/
+func JoinAuthorVoiceChannel(session *discordgo.Session, msg *discordgo.MessageCreate) (*discordgo.VoiceConnection, error) {
+	var vc *discordgo.VoiceConnection
+	// Find sender's voice channel
+	guild, err := session.State.Guild(msg.GuildID)
 	if err != nil {
-		return err
+		return vc, err
 	}
-	defer file.Close()
+	voicechannelID := ""
+	for _, vs := range guild.VoiceStates {
+		if vs.UserID == msg.Author.ID {
+			voicechannelID = vs.ChannelID
+			break
+		}
+	}
+	if voicechannelID == "" {
+		session.ChannelMessageSend(msg.ChannelID, "You're not in a voice channel")
+		return vc, nil
+	}
 
-	// Read mp3 data
+	// Join voice channel and start websocket audio communication
+	vc, err = session.ChannelVoiceJoin(msg.GuildID, voicechannelID, false, true)
+	if err != nil {
+		return vc, err
+	}
+	return vc, nil
+}
+
+/*
+	Play a reader of MP3 data over discord voice connection
+*/
+func PlayMP3(vc *discordgo.VoiceConnection, file io.Reader) error {
+	var wg sync.WaitGroup
+	var err error
+	rawChan := make(chan []int16, 10)
+
+	// Run reader and sender concurrently
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = MP3Reader(rawChan, file)
+	}()
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		err = OpusSender(rawChan, vc)
+	}()
+
+	wg.Wait()
+	return err
+}
+
+/*
+	Decode MP3 data to raw int16 and send in frames of FrameSize through channel
+*/
+func MP3Reader(c chan []int16, file io.Reader) error {
+	// Ensure channel closed after all is read
+	defer close(c)
+	// Set up decoder
 	decoder, err := mp3.NewDecoder(file)
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 
-	var inputAudio [][]int16
-	// Break up into opus frames
+	// Decode and break up into opus frames
 	for {
 		buffer := make([]int16, FrameSize*Channels)
 		err := binary.Read(decoder, binary.LittleEndian, &buffer)
 		if err == io.EOF || err == io.ErrUnexpectedEOF {
-			break
+			c <- buffer
+			return nil
 		}
 		if err != nil {
+			log.Println(err)
 			return err
 		}
-		inputAudio = append(inputAudio, buffer)
+		c <- buffer
 	}
+}
 
-	// Encode with opus and send
+/*
+	Encodes raw audio data from a channel in opus and sends over discord
+*/
+func OpusSender(c chan []int16, vc *discordgo.VoiceConnection) error {
+	// Set up encoder
 	enc, err := gopus.NewEncoder(SampleRate, Channels, gopus.Audio)
 	if err != nil {
+		log.Println(err)
 		return err
 	}
 	enc.SetBitrate(64 * 1000)
-	for i := 0; i < len(inputAudio); i++ {
-		opus, err := enc.Encode(inputAudio[i], FrameSize, FrameSize*Channels*2)
+
+	for {
+		frame, ok := <-c
+		if !ok {
+			// Reached end of channel
+			return nil
+		}
+		// Encode frame and send through opus channel
+		opus, err := enc.Encode(frame, FrameSize, FrameSize*Channels*2)
 		if err != nil {
+			log.Println(err)
 			return err
 		}
 		vc.OpusSend <- opus
 	}
 
-	return nil
 }
--- go.mod
+++ go.mod
@@ -5,9 +5,15 @@ go 1.18
 require github.com/bwmarrin/discordgo v0.25.0
 
 require (
+	github.com/bitly/go-simplejson v0.5.0 // indirect
+	github.com/dlclark/regexp2 v1.4.1-0.20201116162257-a2a8dda75c91 // indirect
+	github.com/dop251/goja v0.0.0-20220516123900-4418d4575a41 // indirect
+	github.com/go-sourcemap/sourcemap v2.1.3+incompatible // indirect
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.3 // indirect
+	github.com/kkdai/youtube/v2 v2.7.15 // indirect
 	golang.org/x/crypto v0.0.0-20210421170649-83a5a9bb288b // indirect
-	golang.org/x/sys v0.0.0-20201119102817-f84b799fce68 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+	golang.org/x/text v0.3.7 // indirect
 	layeh.com/gopus v0.0.0-20210501142526-1ee02d434e32 // indirect
 )
