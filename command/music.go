6fb69c3a8ada3f110f714665088bc3324d9733d4
joeggg <ninjoetsu@gmail.com>
2022-06-02 19:44:56 +0100

    pause, resume, stop commands

--- command/music.go
+++ command/music.go
@@ -22,9 +22,10 @@ var (
 
 var subscriptions = make(map[string]chan string)
 var subCommands = map[string]util.HandlerFunc{
-	"play":  HandlePlay,
-	"pause": HandlePause,
-	"stop":  HandleStop,
+	"play":   HandlePlay,
+	"pause":  HandleEvent,
+	"resume": HandleEvent,
+	"stop":   HandleEvent,
 }
 
 func HandleYT(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
@@ -32,7 +33,7 @@ func HandleYT(session *discordgo.Session, msg *discordgo.MessageCreate, args []s
 		session.ChannelMessageSend(msg.ChannelID, "Unknown music command")
 		return nil
 	} else {
-		return subCommand(session, msg, args[1:])
+		return subCommand(session, msg, args)
 	}
 }
 
@@ -44,32 +45,21 @@ func HandlePlay(session *discordgo.Session, msg *discordgo.MessageCreate, args [
 		session.ChannelMessageSend(msg.ChannelID, "Already playing music")
 		return nil
 	}
-	url := args[0]
-	client := youtube.Client{}
-
-	video, err := client.GetVideo(url)
+	url := args[1]
+	filename, err := DownloadAudio(url)
 	if err != nil {
 		return err
 	}
 
-	format, err := GetFirstOpusFormat(&video.Formats)
-	if err != nil {
-		return err
-	}
-
-	stream, _, err := client.GetStream(video, format)
-	if err != nil {
-		return err
-	}
-	log.Println("Downloading audio file")
-	file, err := SaveWebMStream(stream)
+	file, err := os.Open(filename)
 	if err != nil {
 		return err
 	}
 	defer file.Close()
-	defer os.Remove("test.webm")
+	defer os.Remove(filename)
 
 	eventChan := make(chan string)
+	log.Printf("Creating subscription for user %s", msg.Author.Username)
 	subscriptions[msg.Author.ID] = eventChan
 	vc, err := JoinAuthorVoiceChannel(session, msg)
 	if err != nil {
@@ -77,7 +67,8 @@ func HandlePlay(session *discordgo.Session, msg *discordgo.MessageCreate, args [
 	}
 	// Any error handling past here must close the voice channel connection
 	vc.Speaking(true)
-	err = PlayWebMFile(vc, file)
+	log.Println("Playing track")
+	err = PlayWebMFile(session, msg, vc, eventChan, file)
 	if err != nil {
 		vc.Disconnect()
 		return err
@@ -85,15 +76,53 @@ func HandlePlay(session *discordgo.Session, msg *discordgo.MessageCreate, args [
 
 	vc.Speaking(false)
 	vc.Disconnect()
+	log.Printf("Removing subscription for user %s", msg.Author.Username)
+	delete(subscriptions, msg.Author.ID)
 	return nil
 }
 
-func HandlePause(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
+func HandleEvent(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
+	if _, ok := subscriptions[msg.Author.ID]; !ok {
+		session.ChannelMessageSend(msg.ChannelID, "No music playing")
+		return nil
+	}
+	subscriptions[msg.Author.ID] <- args[0]
 	return nil
 }
 
-func HandleStop(session *discordgo.Session, msg *discordgo.MessageCreate, args []string) error {
-	return nil
+func DownloadAudio(url string) (string, error) {
+	var filename string
+	client := youtube.Client{}
+
+	video, err := client.GetVideo(url)
+	if err != nil {
+		return filename, err
+	}
+
+	format, err := GetFirstOpusFormat(&video.Formats)
+	if err != nil {
+		return filename, err
+	}
+
+	log.Println("Downloading audio file")
+	stream, _, err := client.GetStream(video, format)
+	if err != nil {
+		return filename, err
+	}
+
+	filename = video.ID + ".webm"
+	// Must save to file for webm decoder
+	file, err := os.Create(filename)
+	if err != nil {
+		return filename, err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, stream)
+	if err != nil {
+		return filename, err
+	}
+
+	return filename, nil
 }
 
 /*
@@ -139,44 +168,51 @@ func JoinAuthorVoiceChannel(session *discordgo.Session, msg *discordgo.MessageCr
 	return vc, nil
 }
 
-func SaveWebMStream(stream io.ReadCloser) (*os.File, error) {
-	// Save to file first
-	var file *os.File
-	first, err := os.Create("test.webm")
-	if err != nil {
-		return file, err
-	}
-	_, err = io.Copy(first, stream)
-	if err != nil {
-		return file, err
-	}
-	first.Close()
-	// Reopen needed for some reason
-	file, err = os.Open("test.webm")
-	return file, err
-}
-
-func PlayWebMFile(vc *discordgo.VoiceConnection, file *os.File) error {
+func PlayWebMFile(session *discordgo.Session, msg *discordgo.MessageCreate, vc *discordgo.VoiceConnection, ch chan string, file *os.File) error {
 	// Parse webm
 	var w webm.WebM
 	wr, err := webm.Parse(file, &w)
 	if err != nil {
 		return err
 	}
-	// Read data from parsed webm
+	// Read opus data from parsed webm and pass into sending channel
 	for {
 		// Weird sleep needed to not be faster than the parsing
 		// Shouldn't matter as packets are 20ms long
-		time.Sleep(time.Millisecond)
+		time.Sleep(100 * time.Microsecond)
 		select {
+		case event := <-ch:
+			switch event {
+			case "pause":
+				WaitForResume(ch)
+			case "stop":
+				session.ChannelMessageSend(msg.ChannelID, "Stopping playing")
+				return nil
+			default:
+				continue
+			}
+		// Send the opus data
 		case packet, ok := <-wr.Chan:
 			if !ok {
 				return nil
 			}
-			log.Println(packet.Timecode)
 			vc.OpusSend <- packet.Data
 		default:
 			return nil
+		}
+	}
+}
+
+func WaitForResume(ch chan string) {
+	for {
+		select {
+		case event := <-ch:
+			if event == "resume" {
+				return
+			}
+			time.Sleep(time.Millisecond * 500)
+		default:
+			time.Sleep(time.Millisecond * 500)
 		}
 	}
 }
