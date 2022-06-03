6f83d2d869673d094728cdcf989879a1eb20dbf6
joeggg <ninjoetsu@gmail.com>
2022-06-03 19:09:58 +0100

    added service file + remote install

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,14 @@
+# Assumes token already there and you can scp to /opt/bluebot
 INSTALL_DIR="/opt/bluebot"
-FILES="data/ command/ config/ util/ main.go go.mod go.sum"
+FILES="data/ command/ config/ scripts/bluebot.service util/ main.go go.mod go.sum"
 GO="/usr/local/go/bin/go"
 
 scp -r $FILES $TARGET:$INSTALL_DIR
 # Todo: cross compile and deploy binary?
-ssh $TARGET "cd $INSTALL_DIR; echo \"Building executable\"; $GO build; exit"
-echo "Done!"
+ssh $TARGET "cd $INSTALL_DIR; echo \"Building executable\"; $GO build;"
+echo "Done building"
+
+echo "Installing service"
+ssh $TARGET "sudo mv $INSTALL_DIR/bluebot.service /etc/systemd/system;"\
+            "sudo systemctl daemon-reload; sudo systemctl start bluebot.service"
+echo "Successfully installed service"
